2667ccb37ab652474d21aea51b1156e4a7cbf674
Dusan Dimic <dusan.dimic@vegait.rs>
2024-09-19 14:50:28 +0200

    Chapter 4.5 Validating JSON Input

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/notoriouscode97/greenlight/internal/data"
+	"github.com/notoriouscode97/greenlight/internal/validator"
 	"net/http"
 	"time"
 )
@@ -29,6 +30,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
