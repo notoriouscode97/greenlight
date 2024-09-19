6a3abc3039f0273adc2a54bf0c7ded1a703306f0
Dusan Dimic <dusan.dimic@vegait.rs>
2024-09-19 10:38:19 +0200

    Chapter 3.2 JSON Encoding

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment: %q, "version": %q}`
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
 
-	js = fmt.Sprintf(js, app.config.env, version)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write([]byte(js))
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 }
