adcca47a7283519bda54e4b92131d4380b28f7e2
Dusan Dimic <dusan.dimic@vegait.rs>
2024-10-04 10:38:01 +0200

    Chapter 13.2 Setting up the Users Model

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -8,7 +8,7 @@ import "regexp"
 // reading this in PDF or EPUB format and cannot see the full pattern, please see the
 // note further down the page.
 var (
-	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 )
 
 // Define a new Validator type which contains a map of validation errors.
